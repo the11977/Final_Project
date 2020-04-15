377b10ab2ce8659b34e4259a7f1ddce37b3a29dc
Ronak Chandgadhia <55931910+ronak-ops@users.noreply.github.com>
2020-04-14 20:40:12 -0400

    Third commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-# from send_mail import send_mail
+from send_mail import send_mail
 
 app = Flask(__name__)
 
@@ -19,18 +19,19 @@ db = SQLAlchemy(app)
 
 
 class Feedback(db.Model):
-    __tablename__ = 'feedback'
+    __tableName__ = 'Feedback'
     id = db.Column(db.Integer, primary_key=True)
-    Q1 = db.Column(db.String())
-    Q2 = db.Column(db.String())
-    Q3 = db.Column(db.String())
-    Q4 = db.Column(db.String())
-    Q5 = db.Column(db.String())
-    Q6 = db.Column(db.String())
-    Q7 = db.Column(db.String())
-    
+    Name = db.Column(db.String(200), unique=True)
+    Q1 = db.Column(db.Text)
+    Q2 = db.Column(db.Text)
+    Q3 = db.Column(db.Text)
+    Q4 = db.Column(db.Text)
+    Q5 = db.Column(db.Text)
+    Q6 = db.Column(db.Text)
+    Q7 = db.Column(db.Text)
 
-    def __init__(self, Q1, Q2, Q3, Q4, Q5, Q6, Q7):
+    def __init__(self, Name, Q1, Q2, Q3, Q4, Q5, Q6, Q7):
+        self.Name = Name
         self.Q1 = Q1
         self.Q2 = Q2
         self.Q3 = Q3
@@ -48,29 +49,31 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        Q1 = request.form['Q1']
-        Q2 = request.form['Q2']
-        Q3 = request.form['Q3']
-        Q4 = request.form['Q4']
-        Q5 = request.form['Q5']
-        Q6 = request.form['Q6']
-        Q7 = request.form['Q7']
+        Name = request.form.get('Name')
+        Q1 = request.form.get('Q1')
+        Q2 = request.form.get('Q2')
+        Q3 = request.form.get('Q3')
+        Q4 = request.form.get('Q4')
+        Q5 = request.form.get('Q5')
+        Q6 = request.form.get('Q6')
+        Q7 = request.form.get('Q7')
 
-        #print(Q1, Q2, Q3, Q4, Q5, Q6, Q7)
-         
-    if Q1 == '' or Q2 == '' or Q3 == '' or Q4 == '' or Q5 == '' or Q6 == '' or Q7 == '':
+        # print(Q1, Q2, Q3, Q4, Q5, Q6, Q7)
+
+    if Name == "":
         return render_template('index.html', message='Please enter required fields')
-    
-    if db.session.query(Feedback).filter(Feedback.Q1 == Q1).count() == 0:
-        data = Feedback(Q1, Q2, Q3, Q4, Q5, Q6, Q7)
+    if db.session.query(Feedback).filter(Feedback.Name == Name).count() == 0:
+        data = Feedback(Name, Q1, Q2, Q3, Q4, Q5, Q6, Q7)
         db.session.add(data)
-        db.session.commit()            
-        #send_mail(Q1, Q2, Q3, Q4, Q5, Q6, Q7)
-        return render_template('success.html')
+        db.session.commit()
+        send_mail(Name, Q1, Q2, Q3, Q4, Q5, Q6, Q7)
+    return render_template('success.html')
 
-    return render_template('index.html', message='You have already submitted feedback')
+    return render_template(
+        'index.html',
+        message='You have already submitted feedback'
+        )
 
 
 if __name__ == '__main__':
-#   app.debug = True
     app.run()
--- send_mail.py
+++ send_mail.py
@@ -2,17 +2,17 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def send_mail(customer, dealer, rating, comments):
+def send_mail(Name, Q1, Q2, Q3, Q4, Q5, Q6, Q7):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = ''
-    password = ''
-    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+    login = 'c01bff72d2db58'
+    password = 'c8dbb58be36f46'
+    message = f"<h3>New Response Submission</h3><ul><li>Name: {Name}</li><li>Q1: {Q1}</li><li>Q2: {Q2}</li><li>Q3: {Q3}</li><li>Q4: {Q4}</li><li>Q5: {Q5}</li><li>Q6: {Q6}</li><li>Q7: {Q7}</li></ul>"
 
-    sender_email = 'email1@example.com'
-    receiver_email = 'email2@example.com'
+    sender_email = 'ronak.chandgadhia@gmail.com'
+    receiver_email = 'ronak.chang007@gmail.com'
     msg = MIMEText(message, 'html')
-    msg['Subject'] = 'Lexus Feedback'
+    msg['Subject'] = 'Feedback'
     msg['From'] = sender_email
     msg['To'] = receiver_email
 
